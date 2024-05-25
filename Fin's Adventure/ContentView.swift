e23be02eaf832c1d54d1b24331f537cf03623241
Afrah-saleh <afrahsaleh49@hotmail.com>
2024-05-25 14:55:57 +0300

    Initial Commit

---
+++ Fin's Adventure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fin's Adventure
+//
+//  Created by Afrah Saleh on 17/11/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fin's Adventure/Fin_s_AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fin_s_AdventureApp.swift
+//  Fin's Adventure
+//
+//  Created by Afrah Saleh on 17/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Fin_s_AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
